694ccb6d279591ade45a7bdaec71a91115668e35
SargePopwell <crenier.luc@gmail.com>
2021-05-21 09:31:03 +0200

    Ajout livre2, livre3, et var=livres

--- JS/script.js
+++ JS/script.js
@@ -4,4 +4,22 @@ var livre1 = {
     reference: "05",
     titre: "L'alchimiste",
     auteur: "Paolo Coelho"
-}
+};
+
+var livre2 = {
+    reference: "01",
+    titre: "L'existentialisme est un humanisme",
+    auteur: "Jean-Paul Sartre"
+};
+
+var livre3 = {
+    reference: "02",
+    titre: "Voyage au bout de la nuit",
+
+};
+
+var livres = [livre1, livre2, livre3];
+
+console.log(livres);
+
+function mkHTML(
