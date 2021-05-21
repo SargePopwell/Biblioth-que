066e5dae1a65453958283c02fd4570b3269d617e
SargePopwell <crenier.luc@gmail.com>
2021-05-21 10:08:55 +0200

    Ajout fonction mkHtml, var htmlString et conteneur

--- JS/script.js
+++ JS/script.js
@@ -1,25 +1,37 @@
 "use.strict"
 
 var livre1 = {
-    reference: "05",
+    reference: "1",
     titre: "L'alchimiste",
     auteur: "Paolo Coelho"
 };
 
 var livre2 = {
-    reference: "01",
+    reference: "2",
     titre: "L'existentialisme est un humanisme",
     auteur: "Jean-Paul Sartre"
 };
 
 var livre3 = {
-    reference: "02",
+    reference: "3",
     titre: "Voyage au bout de la nuit",
-
+    auteur: "Louis-Ferdinand Celine"
 };
 
 var livres = [livre1, livre2, livre3];
+var conteneur = document.getElementById("liste");
 
 console.log(livres);
 
-function mkHTML(
+var htmlString = "";
+
+function mkHtml(liste) {
+
+    for(var livre of liste) {
+        htmlString += "<ul class=\"liste\">";
+        htmlString += "<li class=\"livre\">\"" + livre.titre + "\", par \"" + livre.auteur + "\".</li>";
+        htmlString += "</ul>";
+    }
+}
+mkHtml(livres);
+conteneur.innerHTML = htmlString;
